b2f38f6f089ccee5d5e61b550ed8e752aac000a5
xiangyangkan <xiangyangkan@outlook.com>
2024-05-29 13:21:07 +0800

    feat: update to 24.05

--- docker/build.sh
+++ docker/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 WORKING_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+ENABLE_PRUNE="true"
+
+function docker_prune() {
+    if [ "$ENABLE_PRUNE" = "true" ]; then
+      docker system prune -a -f || exit 1
+    fi
+}
 
 function build_pytorch_image() {
     local ngc_version="$1"
@@ -7,7 +14,7 @@ function build_pytorch_image() {
     local base_image="nvcr.io/nvidia/pytorch:$ngc_version-py3"
     docker build --target devel --build-arg BASE_IMAGE="$base_image" --build-arg PYTHON_VERSION="$python_version" \
       -t rivia/pytorch:"$ngc_version" -f Dockerfile . || exit 1
-    docker push rivia/pytorch:"$ngc_version" && docker system prune -a -f
+    docker push rivia/pytorch:"$ngc_version" && docker_prune
 }
 
 function build_tensorflow_image() {
@@ -16,7 +23,7 @@ function build_tensorflow_image() {
     local base_image="nvcr.io/nvidia/tensorflow:$ngc_version-tf2-py3"
     docker build --target devel --build-arg BASE_IMAGE="$base_image" --build-arg PYTHON_VERSION="$python_version" \
       -t rivia/tensorflow:"$ngc_version" -f Dockerfile . || exit 1
-    docker push rivia/tensorflow:"$ngc_version" && docker system prune -a -f
+    docker push rivia/tensorflow:"$ngc_version" && docker_prune
 }
 
 function build_triton_server_image() {
@@ -24,7 +31,7 @@ function build_triton_server_image() {
     local base_image="nvcr.io/nvidia/tritonserver:$ngc_version-py3"
     docker build --target base --build-arg BASE_IMAGE="$base_image" \
       -t rivia/tritonserver:"$ngc_version" -f Dockerfile . || exit 1
-    docker push rivia/tritonserver:"$ngc_version" && docker system prune -a -f
+    docker push rivia/tritonserver:"$ngc_version" && docker_prune
 }
 
 function build_tensorrt_image() {
@@ -39,7 +46,7 @@ function build_tensorrt_image() {
     docker build --target build --build-arg BASE_IMAGE="$stage_image" \
       --build-arg CMAKE_VERSION="$cmake_version" --build-arg BAZELISK_VERSION="$bazelisk_version" \
       -t rivia/tensorrt:"$ngc_version" -f Dockerfile . || exit 1
-    docker push rivia/tensorrt:"$ngc_version" && docker system prune -a -f
+    docker push rivia/tensorrt:"$ngc_version" && docker_prune
 }
 
 function build_trtllm_image() {
@@ -59,28 +66,29 @@ function build_trtllm_image() {
       -t $stage_image -f Dockerfile . || exit 1
     docker build --target tensorrt --build-arg BASE_IMAGE="$stage_image" \
       -t rivia/tensorrt_llm:"$trtllm_version" -f Dockerfile . || exit 1
-    docker push rivia/tensorrt_llm:"$trtllm_version" && docker system prune -a -f
+    docker push rivia/tensorrt_llm:"$trtllm_version" && docker_prune
 }
 
-function build_trtllm_backend_base_image() {
+function build_trtllm_backend_from_scratch() {
     local ngc_version="$1"
     local trtllm_version="$2"
     local tensorrt_version="$3"
     rm -rf general/tensorrtllm_backend
     rm -rf general/server
-    git clone -b "v$trtllm_version" https://github.com/triton-inference-server/tensorrtllm_backend.git general/tensorrtllm_backend
-    git clone -b "r$ngc_version" https://github.com/triton-inference-server/server.git general/server
+    # 使用`triton-inference-server/tensorrtllm_backend`的主分支最新代码
+    git clone https://github.com/triton-inference-server/tensorrtllm_backend.git general/tensorrtllm_backend
+    # 使用`triton-inference-server/server`的主分支最新代码
+    git clone https://github.com/triton-inference-server/server.git general/server
     cd "$WORKING_DIR/general/tensorrtllm_backend" || exit 1
     git submodule update --init --recursive
     apt install -y git-lfs && git lfs pull || exit 1
-#    docker build --build-arg BASE_TAG="$ngc_version-pyt-python-py3" \
-#      -t "nvcr.io/nvidia/tritonserver:$ngc_version-trtllm-python-py3" -f dockerfile/Dockerfile.trt_llm_backend .
 
     BASE_IMAGE="nvcr.io/nvidia/pytorch:${ngc_version}-py3"
-    TRT_URL_x86="https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/${tensorrt_version%.*}/tensorrt-${tensorrt_version}.linux.x86_64-gnu.cuda-12.2.tar.gz"
-    TRT_URL_ARM="https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/${tensorrt_version%.*}/tensorrt-${tensorrt_version}.ubuntu-22.04.aarch64-gnu.cuda-12.2.tar.gz"
+    TRT_URL_x86="https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/${tensorrt_version%.*}/tars/TensorRT-${tensorrt_version}.Linux.x86_64-gnu.cuda-12.4.tar.gz"
+    TRT_URL_ARM="https://developer.nvidia.com/downloads/compute/machine-learning/tensorrt/${tensorrt_version%.*}/tars/TensorRT-${tensorrt_version}.ubuntu-22.04.aarch64-gnu.cuda-12.4.tar.gz"
     TRTLLM_BASE_IMAGE=trtllm_base
-    TENSORRTLLM_BACKEND_REPO_TAG=rel
+    # 使用`TensorRT-LLM`的主分支最新代码编译
+    TENSORRTLLM_BACKEND_REPO_TAG=main
     docker build -t ${TRTLLM_BASE_IMAGE} \
                  --build-arg BASE_IMAGE="${BASE_IMAGE}" \
                  --build-arg TRT_VER="${tensorrt_version}" \
@@ -98,7 +106,14 @@ function build_trtllm_backend_base_image() {
               --image=base,${TRTLLM_BASE_IMAGE} \
               --backend=tensorrtllm:${TENSORRTLLM_BACKEND_REPO_TAG} \
               --backend=python:"r${ngc_version}" || exit 1
-    docker tag tritonserver:latest "nvcr.io/nvidia/tritonserver:$ngc_version-trtllm-python-py3"
+
+    local stage_image="triton_backend:base"
+    local tag="latest-trtllm"
+    docker build --target base --build-arg BASE_IMAGE="tritonserver:latest" \
+      -t $stage_image -f Dockerfile . || exit 1
+    docker build --target devel --build-arg BASE_IMAGE="$stage_image" --build-arg PYTHON_VERSION="$python_version" \
+      -t rivia/triton_backend:"$tag" -f Dockerfile . || exit 1
+    docker push rivia/triton_backend:"$tag" && docker_prune
 }
 
 function build_triton_backend_image() {
@@ -127,7 +142,7 @@ function build_triton_backend_image() {
       -t $stage_image -f Dockerfile . || exit 1
     docker build --target devel --build-arg BASE_IMAGE="$stage_image" --build-arg PYTHON_VERSION="$python_version" \
       -t rivia/triton_backend:"$tag" -f Dockerfile . || exit 1
-    docker push rivia/triton_backend:"$tag" && docker system prune -a -f
+    docker push rivia/triton_backend:"$tag" && docker_prune
 }
 
 function build_deepstream_image() {
@@ -146,7 +161,7 @@ function build_deepstream_image() {
     docker build --target deepstream --build-arg BASE_IMAGE="$stage_image" --build-arg ARCH="$arch" \
       --build-arg DEEPSTREAM_VERSION="$deepstream_version" --build-arg PYDS_VERSION="$pyds_version" \
       -t rivia/deepstream:"$deepstream_version" -f Dockerfile . || exit 1
-    docker push rivia/deepstream:"$deepstream_version" && docker system prune -a -f
+    docker push rivia/deepstream:"$deepstream_version" && docker_prune
 }
 
 function build_nemo_image() {
@@ -155,7 +170,7 @@ function build_nemo_image() {
     local base_image="nvcr.io/nvidia/nemo:$ngc_version"
     docker build --target devel --build-arg BASE_IMAGE="$base_image" --build-arg PYTHON_VERSION="$python_version" \
       -t rivia/nemo:"$ngc_version" -f Dockerfile . || exit 1
-    docker push rivia/nemo:"$ngc_version" && docker system prune -a -f
+    docker push rivia/nemo:"$ngc_version" && docker_prune
 }
 
 function build_lmdeploy_image() {
@@ -164,11 +179,11 @@ function build_lmdeploy_image() {
     local base_image="openmmlab/lmdeploy:v$lmdeploy_version"
     docker build --target devel --build-arg BASE_IMAGE="$base_image" --build-arg PYTHON_VERSION="$python_version" \
       -t "rivia/pytorch:lmdeploy-$lmdeploy_version" -f Dockerfile . || exit 1
-    docker push "rivia/pytorch:lmdeploy-$lmdeploy_version" && docker system prune -a -f
+    docker push "rivia/pytorch:lmdeploy-$lmdeploy_version" && docker_prune
 }
 
 
-NGC_VERSION="24.04"
+NGC_VERSION="24.05"
 PYTHON_VERSION="3.10"
 CMAKE_VERSION="3.28.4"
 BAZELISK_VERSION="1.19.0"
@@ -177,10 +192,11 @@ DEEPSTREAM_VERSION="6.4-triton-multiarch"
 JETSON_VERSION="r36.2.0"
 PYDS_VERSION="1.1.10"
 TRTLLM_VERSION="0.9.0"
-TENSORRT_VERSION="9.2.0.5"
+TENSORRT_VERSION="10.0.1.6"
 LMDEPLOY_VERSION="0.4.2"
 CUSTOM_TRTLLM_BACKEND="true"
 dos2unix ./*
+
 build_pytorch_image "$NGC_VERSION" "$PYTHON_VERSION" || exit 1
 build_tensorflow_image "$NGC_VERSION" "$PYTHON_VERSION" || exit 1
 build_triton_server_image "$NGC_VERSION" || exit 1
